5eb0618651f3094f54f17614e248baf8e5531e61
Viljamas Grumadas <viljamasgr@gmail.com>
2025-06-06 13:37:23 +0100

    Finished dice roll program

--- Learning-Java/src/projects/dice_roller_program.java
+++ Learning-Java/src/projects/dice_roller_program.java
@@ -13,18 +13,88 @@ public class dice_roller_program {
 
         // Declare variables
         int numOfDice;
-        int total;
+        int total = 0;
 
         // Get # of dive from the user
         System.out.print("Enter the # of dice to roll: ");
         numOfDice = scanner.nextInt();
 
-        // Check ig # of dice > 0
+        // Check if # of dice > 0
+        if(numOfDice > 0){
+            for(int i = 0; i < numOfDice; i++){
+                // Roll all the dice
+                int roll = random.nextInt(1, 7);
+                printDie(roll);
+                System.out.println("You rolled: " + roll);
+                // Get the total
+                total += roll;
+            }
+            System.out.println("Total: " + total);
+        }
+        else{
+            System.out.println("# of dice mut be greater then 0.");
+        }
+    }
+    // Display ASCII of dice
+    static void printDie(int roll){
 
-        // Roll all the dice
+        String dice1 = """
+        ╭───────╮
+        │       │
+        │   ●   │
+        │       │
+        ╰───────╯
+        """;
 
-        // Get the total
+        String dice2 = """
+        ╭───────╮
+        │ ●     │
+        │       │
+        │     ● │
+        ╰───────╯
+        """;
 
-        // Display ASCII of dice
+        String dice3 = """
+        ╭───────╮
+        │ ●     │
+        │   ●   │
+        │     ● │
+        ╰───────╯
+        """;
+
+        String dice4 = """
+        ╭───────╮
+        │ ●   ● │
+        │       │
+        │ ●   ● │
+        ╰───────╯
+        """;
+
+        String dice5 = """
+        ╭───────╮
+        │ ●   ● │
+        │   ●   │
+        │ ●   ● │
+        ╰───────╯
+        """;
+
+        String dice6 = """
+        ╭───────╮
+        │ ●   ● │
+        │ ●   ● │
+        │ ●   ● │
+        ╰───────╯
+        """;
+
+        switch (roll){
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.println("Invalid roll");
+        }
+
     }
 }
